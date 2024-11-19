420bfba6bd887f7be07a5a7dfa00478a7173f6b4
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-18 21:46:03 -0300

    feat: development of use case

--- src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -1,3 +1,5 @@
+import AddProductUseCase from "./add-product.usecase";
+
 const MockRepository = () => {
   return {
     add: jest.fn(),
@@ -16,6 +18,13 @@ describe("Add product use case unit test", () => {
       purchasePrice: 100,
       stock: 10,
     };
-    useCase.execute(input);
+    const result = await useCase.execute(input);
+
+    expect(productRepository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined();
+    expect(result.name).toBe(input.name);
+    expect(result.description).toBe(input.description);
+    expect(result.purchasePrice).toBe(input.purchasePrice);
+    expect(result.stock).toBe(input.stock);
   });
 });
