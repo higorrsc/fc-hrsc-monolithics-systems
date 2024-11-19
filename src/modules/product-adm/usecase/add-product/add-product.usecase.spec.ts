a1cd61526ac29c2000c7552450fa1fc602487987
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-18 21:35:00 -0300

    feat: add test for use case

---
+++ src/modules/product-adm/usecase/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,21 @@
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+};
+
+describe("Add product use case unit test", () => {
+  it("should add a product", async () => {
+    const productRepository = MockRepository();
+    const useCase = new AddProductUseCase(productRepository);
+
+    const input = {
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+    useCase.execute(input);
+  });
+});
