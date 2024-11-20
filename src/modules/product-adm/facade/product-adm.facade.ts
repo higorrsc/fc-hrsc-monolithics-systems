effcf65c18a7a12c4656374f9b720a26449217e0
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-20 20:41:30 -0300

    feat: implements product adm facade

---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,36 @@
+import UseCaseInterface from "../../@shared/usecase/use-case.interface";
+import {
+  AddProductFacadeInputDto,
+  CheckStockFacadeInputDto,
+  CheckStockFacadeOutputDto,
+} from "./product-adm.facade.dto";
+import ProductAdmFacadeInterface from "./product-adm.facade.interface";
+
+export interface UseCaseProps {
+  addUseCase: UseCaseInterface;
+  stockUseCase: UseCaseInterface;
+}
+
+export default class ProductAdmFacade implements ProductAdmFacadeInterface {
+  private _addUseCase: UseCaseInterface;
+  private _checkStockUseCase: UseCaseInterface;
+
+  constructor(useCasesProps: UseCaseProps) {
+    this._addUseCase = useCasesProps.addUseCase;
+    this._checkStockUseCase = useCasesProps.stockUseCase;
+  }
+
+  addProduct(input: AddProductFacadeInputDto): Promise<void> {
+    // caso o DTO do caso de uso for != do DTO da facade
+    // converter DTO da facade para o DTO do caso de uso
+    return this._addUseCase.execute(input);
+  }
+
+  checkStock(
+    input: CheckStockFacadeInputDto
+  ): Promise<CheckStockFacadeOutputDto> {
+    // caso o DTO do caso de uso for != do DTO da facade
+    // converter DTO da facade para o DTO do caso de uso
+    return this._checkStockUseCase.execute(input);
+  }
+}
