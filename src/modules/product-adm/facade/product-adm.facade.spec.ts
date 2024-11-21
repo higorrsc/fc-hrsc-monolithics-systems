53d82cd5033e3ebff9e318c68305cbe10d124f24
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-20 21:21:10 -0300

    feat: add check stock use case with tests

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -45,4 +45,19 @@ describe("ProductAdmFacade test", () => {
     expect(product.purchasePrice).toBe(input.purchasePrice);
     expect(product.stock).toBe(input.stock);
   });
+
+  it("should check stock", async () => {
+    const productFacade = ProductAdmFacadeFactory.create();
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 10,
+      stock: 10,
+    };
+    await productFacade.addProduct(input);
+    const result = await productFacade.checkStock({ productId: input.id });
+    expect(result.productId).toBe(input.id);
+    expect(result.stock).toBe(input.stock);
+  });
 });
---
+++ src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,30 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity";
+import CheckStockUseCase from "./check-stock.usecase";
+
+const product = new Product({
+  id: new Id("1"),
+  name: "Product 1",
+  description: "Description 1",
+  purchasePrice: 100,
+  stock: 10,
+});
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+  };
+};
+
+describe("Check Stock use case unit test", () => {
+  it("should check stock", async () => {
+    const productRepository = MockRepository();
+    const useCase = new CheckStockUseCase(productRepository);
+    const input = { productId: "1" };
+    const result = await useCase.execute(input);
+    expect(productRepository.find).toHaveBeenCalled();
+    expect(result.productId).toBe("1");
+    expect(result.stock).toBe(10);
+  });
+});
