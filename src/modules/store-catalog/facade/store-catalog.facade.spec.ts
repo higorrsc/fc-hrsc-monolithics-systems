7e0be9933b8aa3a3c4492ee442f0b85f4f104dc8
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-21 20:42:02 -0300

    feat: finish store catalog facade implementation

--- src/modules/store-catalog/facade/store-catalog.facade.spec.ts
+++ src/modules/store-catalog/facade/store-catalog.facade.spec.ts
@@ -29,10 +29,36 @@ describe("StoreCatalogFacade test", () => {
       description: "Product 1 description",
       salesPrice: 100,
     });
-    const result = await facade.findProduct({ id: product.id });
+    const result = await facade.find({ id: product.id });
     expect(result.id).toBe(product.id);
     expect(result.name).toBe(product.name);
     expect(result.description).toBe(product.description);
     expect(result.salesPrice).toBe(product.salesPrice);
+  });
+
+  it("should find all products", async () => {
+    const facade = StoreCatalogFacadeFactory.create();
+    const product = await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      salesPrice: 100,
+    });
+    const product2 = await ProductModel.create({
+      id: "2",
+      name: "Product 2",
+      description: "Product 2 description",
+      salesPrice: 200,
+    });
+    const result = await facade.findAll();
+    expect(result.products.length).toBe(2);
+    expect(result.products[0].id).toBe(product.id);
+    expect(result.products[0].name).toBe(product.name);
+    expect(result.products[0].description).toBe(product.description);
+    expect(result.products[0].salesPrice).toBe(product.salesPrice);
+    expect(result.products[1].id).toBe(product2.id);
+    expect(result.products[1].name).toBe(product2.name);
+    expect(result.products[1].description).toBe(product2.description);
+    expect(result.products[1].salesPrice).toBe(product2.salesPrice);
   });
 });
