7bb4d6966f25b489f8b5b84767fa239518df0970
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-21 21:50:20 -0300

    feat: implements client adm facade interface and DTO

---
+++ src/modules/client-adm/facade/client-adm.facade.interface.ts
@@ -0,0 +1,13 @@
+import {
+  AddClientFacadeInputDto,
+  FindClientFacadeInputDto,
+  FindClientFacadeOutputDto,
+} from "./client-adm.facade.dto";
+
+export default interface ClientAdmFacadeInterface {
+  addClient(input: AddClientFacadeInputDto): Promise<void>;
+
+  findClient(
+    input: FindClientFacadeInputDto
+  ): Promise<FindClientFacadeOutputDto>;
+}
