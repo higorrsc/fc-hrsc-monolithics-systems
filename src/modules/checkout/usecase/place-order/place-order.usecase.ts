16257d67c8804f89233a9b62a9ba011d0bd05e8c
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-23 11:08:13 -0300

    feat: implement product validation

--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,12 +1,18 @@
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import ClientAdmFacadeInterface from "../../../client-adm/facade/client-adm.facade.interface";
+import ProductAdmFacadeInterface from "../../../product-adm/facade/product-adm.facade.interface";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
   private _clientFacade: ClientAdmFacadeInterface;
+  private _productFacade: ProductAdmFacadeInterface;
 
-  constructor(clientFacade: ClientAdmFacadeInterface) {
+  constructor(
+    clientFacade: ClientAdmFacadeInterface,
+    productFacade: ProductAdmFacadeInterface
+  ) {
     this._clientFacade = clientFacade;
+    this._productFacade = productFacade;
   }
 
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -16,6 +22,7 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
       throw new Error("Client not found");
     }
     // validar produtos
+    await this.validateProducts(input);
     // recuperar os produtos
 
     // criar o objeto do client
@@ -34,5 +41,22 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
       total: 0,
       products: [],
     };
+  }
+
+  private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
+    if (input.products.length === 0) {
+      throw new Error("No products select");
+    }
+
+    for (const p of input.products) {
+      const product = await this._productFacade.checkStock({
+        productId: p.productId,
+      });
+      if (product.stock <= 0) {
+        throw new Error(
+          `Product ${product.productId} is not available in stock`
+        );
+      }
+    }
   }
 }
