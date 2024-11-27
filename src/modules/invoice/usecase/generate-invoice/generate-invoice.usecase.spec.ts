c6490ac95fe97224f2393f672f0d567480f5ba10
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-26 21:14:31 -0300

    feat: implement generate invoice usecase

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.spec.ts
@@ -0,0 +1,48 @@
+import GenerateInvoiceUseCase from "./generate-invoice.usecase";
+
+const MockRepository = () => {
+  return {
+    generate: jest.fn(),
+    find: jest.fn(),
+  };
+};
+describe("should create an invoice", () => {
+  it("should create an invoice", async () => {
+    const repository = MockRepository();
+    const usecase = new GenerateInvoiceUseCase(repository);
+    const input = {
+      name: "Janie Patton",
+      document: "ABC123",
+      street: "Helena Fuller",
+      number: "54",
+      complement: "AP 123",
+      city: "Belo Horizonte",
+      state: "MG",
+      zipCode: "31659-564",
+      items: [
+        {
+          id: "1",
+          name: "Product 1",
+          price: 10,
+        },
+        {
+          id: "2",
+          name: "Product 2",
+          price: 20,
+        },
+      ],
+    };
+    const result = await usecase.execute(input);
+    expect(repository.generate).toHaveBeenCalled();
+    expect(result.id).toBeDefined();
+    expect(result.name).toEqual(input.name);
+    expect(result.document).toEqual(input.document);
+    expect(result.street).toEqual(input.street);
+    expect(result.number).toEqual(input.number);
+    expect(result.complement).toEqual(input.complement);
+    expect(result.city).toEqual(input.city);
+    expect(result.state).toEqual(input.state);
+    expect(result.zipCode).toEqual(input.zipCode);
+    expect(result.items).toHaveLength(2);
+  });
+});
---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.ts
@@ -0,0 +1,65 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Address from "../../domain/address.value-object";
+import InvoiceItem from "../../domain/invoice-item.entity";
+import Invoice from "../../domain/invoice.entity";
+import InvoiceGateway from "../../gateway/invoice.gateway";
+import {
+  GenerateInvoiceInputDto,
+  GenerateInvoiceOutputDto,
+} from "./generate-invoice.dto";
+
+export default class GenerateInvoiceUseCase {
+  private _invoiceRepository: InvoiceGateway;
+
+  constructor(invoiceRepository: InvoiceGateway) {
+    this._invoiceRepository = invoiceRepository;
+  }
+
+  async execute(
+    input: GenerateInvoiceInputDto
+  ): Promise<GenerateInvoiceOutputDto> {
+    const invoiceId = new Id(input.id) || new Id();
+    const props = {
+      id: invoiceId,
+      name: input.name,
+      document: input.document,
+      address: new Address({
+        street: input.street,
+        number: input.number,
+        complement: input.complement,
+        city: input.city,
+        state: input.state,
+        zipCode: input.zipCode,
+      }),
+      items: input.items.map((i) => {
+        return new InvoiceItem({
+          id: new Id(i.id) || new Id(),
+          invoiceId: invoiceId,
+          name: i.name,
+          price: i.price,
+        });
+      }),
+    };
+    const invoice = new Invoice(props);
+    this._invoiceRepository.generate(invoice);
+    return {
+      id: invoice.id.id,
+      name: invoice.name,
+      document: invoice.document,
+      street: invoice.address.street,
+      number: invoice.address.number,
+      complement: invoice.address.complement,
+      city: invoice.address.city,
+      state: invoice.address.state,
+      zipCode: invoice.address.zipCode,
+      items: invoice.items.map((i) => {
+        return {
+          id: i.id.id,
+          name: i.name,
+          price: i.price,
+        };
+      }),
+      total: invoice.items.reduce((total, item) => total + item.price, 0),
+    };
+  }
+}
