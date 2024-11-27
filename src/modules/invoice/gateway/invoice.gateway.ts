9e2fb2afa8208f22b5586aa762ac0268319d7c23
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-11-26 21:31:28 -0300

    feat: add find method to invoice gateway

--- src/modules/invoice/gateway/invoice.gateway.ts
+++ src/modules/invoice/gateway/invoice.gateway.ts
@@ -2,4 +2,5 @@ import Invoice from "../domain/invoice.entity";
 
 export default interface InvoiceGateway {
   generate(input: Invoice): Promise<Invoice>;
+  find(input: string): Promise<Invoice>;
 }
