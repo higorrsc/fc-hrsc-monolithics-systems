5477c66e72cb068af8636adde0669695bc2415f6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-01-12 22:18:18 -0300

    feat: add invoice facade at route to get data

--- src/infrastructure/api/routes/invoice.route.ts
+++ src/infrastructure/api/routes/invoice.route.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express'
-import InvoiceRepository from '../../../modules/invoice/repository/invoice.repository'
-import FindInvoiceUseCase from '../../../modules/invoice/usecase/find-invoice/find-invoice.usecase'
+import InvoiceFacadeInterface from '../../../modules/invoice/facade/invoice.facade.interface'
+import InvoiceFacadeFactory from '../../../modules/invoice/factory/invoice.facade.factory'
 
 export const invoiceRoute = express.Router()
 
-invoiceRoute.get('/:id', (req: Request, res: Response) => {
-  const useCase = new FindInvoiceUseCase(new InvoiceRepository())
+invoiceRoute.get('/:id', async (req: Request, res: Response) => {
   try {
-    const output = useCase.execute({ id: req.params.id })
+    const invoiceFacade: InvoiceFacadeInterface = InvoiceFacadeFactory.create()
+    const output = await invoiceFacade.find({
+      id: req.params.id,
+    })
     res.status(200).send(output)
   } catch (err) {
     res.status(500).send(err)
