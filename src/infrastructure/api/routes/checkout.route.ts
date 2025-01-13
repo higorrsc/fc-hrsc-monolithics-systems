6968c41997579c482b2655ae5fb7c02228be6257
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-01-12 21:40:16 -0300

    feat: remove DTO and pass request body to facade

--- src/infrastructure/api/routes/checkout.route.ts
+++ src/infrastructure/api/routes/checkout.route.ts
@@ -1,19 +1,13 @@
 import express, { Request, Response } from 'express'
 import CheckoutFacadeInterface from '../../../modules/checkout/facade/checkout.facade.interface'
 import CheckoutFacadeFactory from '../../../modules/checkout/factory/checkout.facade.factory'
-import { PlaceOrderInputDto } from '../../../modules/checkout/usecase/place-order/place-order.dto'
 
 export const checkoutRoute = express.Router()
 checkoutRoute.post('/', async (req: Request, res: Response) => {
   try {
     const checkoutFacade: CheckoutFacadeInterface =
       CheckoutFacadeFactory.create()
-
-    const input: PlaceOrderInputDto = {
-      clientId: req.body.clientId,
-      products: req.body.products,
-    }
-    const output = await checkoutFacade.placeOrder(input)
+    const output = await checkoutFacade.placeOrder(req.body)
     res.status(201).send(output)
   } catch (err) {
     res.status(500).send(err)
